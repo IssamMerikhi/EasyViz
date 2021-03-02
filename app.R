0f28870fc916183be1074218c5147d8c54513681
Issam Merikhi <47690579+IssamMerikhi@users.noreply.github.com>
2021-03-02 16:31:52 +0100

    add a second page for heatmap

--- app.R
+++ app.R
@@ -2,6 +2,18 @@ library(tidyr)
 library(ggplot2)
 library(shiny)
 library(shinydashboard)
+library(heatmaply)
+
+df.team_data <- expand.grid(teams = c("Team A", "Team B", "Team C", "Team D")
+                            ,metrics = c("Metric 1", "Metric 2", "Metric 3", "Metric 4", "Metric 5")
+)
+# add variable: performance
+set.seed(41)
+df.team_data$performance <- rnorm(nrow(df.team_data))
+
+
+
+
 
 
 header <- dashboardHeader(title = "First RShiny Dashboard")
@@ -9,8 +21,8 @@ header <- dashboardHeader(title = "First RShiny Dashboard")
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    menuItem("Histogram", tabName = "hist", icon = icon("dashboard")),
+    menuItem("Heatmap", tabName = "heat", icon = icon("th"))
     )
   )
 
@@ -18,9 +30,13 @@ sidebar <- dashboardSidebar(
 
 body <- dashboardBody(
   tabItems(
-    tabItem(tabName = "dashboard",
-            fluidRow(box(plotOutput("plot1", height = 500),
-                         title = "Histogram")))
+    tabItem(tabName = "hist",
+            fluidRow(box(plotOutput("plot1", height = 400, width = 400),
+                         title = "Histogram"))),
+    
+    tabItem(tabName = "heat",
+            fluidRow(box(plotOutput("plot2", height = 400, width = 400),
+                         title = "Heatmap")))
     )
   )
 
@@ -44,6 +60,10 @@ server <- function(input, output) {
       geom_bar() + 
       facet_wrap(~key, scales = 'free_x')
   })
+  
+  
+  output$plot2 <- renderPlot({ggplot(data = df.team_data, aes(x = metrics, y = teams)) +
+      geom_tile(aes(fill = performance)) })
   
 }
 
