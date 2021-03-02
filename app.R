eb26c2ede08ada5e24fb9983c56b232358a517fe
Issam Merikhi <47690579+IssamMerikhi@users.noreply.github.com>
2021-03-02 16:17:08 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -1,20 +1,52 @@
-df = read.csv("C:/Users/Elève/Desktop/PL.csv")
 library(tidyr)
 library(ggplot2)
 library(shiny)
 library(shinydashboard)
 
-ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
-)
 
-server <- function(input, output) { }
+header <- dashboardHeader(title = "First RShiny Dashboard")
+
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  )
+
+
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "dashboard",
+            fluidRow(box(plotOutput("plot1", height = 500),
+                         title = "Histogram")))
+    )
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+ui <- dashboardPage(header, sidebar, body)
+
+server <- function(input, output) {
+  
+  output$plot1 <- renderPlot({
+    ggplot(gather(mtcars), aes(value)) + 
+      geom_bar() + 
+      facet_wrap(~key, scales = 'free_x')
+  })
+  
+}
 
 shinyApp(ui, server)
 
 
-ggplot(gather(mtcars), aes(value)) + 
-  geom_bar() + 
-  facet_wrap(~key, scales = 'free_x')
