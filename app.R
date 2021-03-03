c35ff31d783902f76d12bcef81bd5baf25745b01
Issam Merikhi <47690579+IssamMerikhi@users.noreply.github.com>
2021-03-03 16:15:03 +0100

    source('cleandata.r')

--- app.R
+++ app.R
@@ -3,6 +3,9 @@ library(ggplot2)
 library(shiny)
 library(shinydashboard)
 library(heatmaply)
+source("cleandata.r")
+
+
 
 df.team_data <- expand.grid(teams = c("Team A", "Team B", "Team C", "Team D")
                             ,metrics = c("Metric 1", "Metric 2", "Metric 3", "Metric 4", "Metric 5")
@@ -75,7 +78,8 @@ server <- function(input, output) {
     if(is.null(file_to_read)){
       return()
     }
-    read.csv(file_to_read$datapath)
+    c = read.csv(file_to_read$datapath)
+    cleanData(c)
   })
   
   
