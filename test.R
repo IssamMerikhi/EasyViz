06badfb2c960b2008141a270dff562829293f3b1
Issam Merikhi <47690579+IssamMerikhi@users.noreply.github.com>
2021-03-03 14:14:36 +0100

    Delete test.R

--- test.R
+++
@@ -1,8 +0,0 @@
-df = read.csv("C:/Users/Elève/Desktop/PL.csv")
-library(tidyr)
-library(ggplot2)
-
-head(mtcars)
-ggplot(gather(mtcars), aes(value)) + 
-  geom_bar() + 
-  facet_wrap(~key, scales = 'free_x')
