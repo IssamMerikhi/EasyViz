b53a2119677c6ab8ebd49437dc9531addad9ab10
Issam Merikhi <47690579+IssamMerikhi@users.noreply.github.com>
2021-03-05 17:01:29 +0100

    Add files via upload

--- graphics.r
+++ graphics.r
@@ -1,24 +1,29 @@
 library(ggplot2)
 library(reshape2)
 library(zoo)
+library(igraph)
+library(networkD3)
 source("cleandata.r")
+
 
 
 
 bargraph <- function(yourData){
   
-  bplot = yourData
-  bplot = as.numeric(bplot)
-  barplot(bplot)
+  df = yourData
+  df = cleanData(df)
+  df = zoo(df)
+  barplot(df, main = "Densities", beside = TRUE)
   
-} 
+}
 
 heatgraph <- function(yourData){
   
   cormat <- round(cor(yourData),2)
   melted_cormat <- melt(cormat)
-  ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
-    geom_tile()
+  ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value), title("Heatmap")) + 
+    geom_tile() + ggtitle("Heatmap") +
+    xlab("Features") + ylab("Features")
   
 }
 
@@ -26,7 +31,7 @@ densgraph <- function(yourData){
   df = yourData
   df = cleanData(df)
   df = zoo(df)
-  plot(df)
+  plot(df, main = "Densities")
   
 }
 
