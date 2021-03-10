fa54bf3857a465386b1f7ff917dbc1f838e25479
Issam Merikhi <47690579+IssamMerikhi@users.noreply.github.com>
2021-03-11 00:19:48 +0100

    necessary for deployment

---
+++ deployment.r
@@ -0,0 +1,5 @@
+rsconnect::setAccountInfo(name='issammerikhi',
+                          token='AFB0405ACB380E3357069C1FF8F5D3C0',
+                          secret='5rf7wlAXVPulbKbOpRLosVIjqgOnTLnqEeO7dHQ+')
+library(rsconnect)
+rsconnect::deployApp("C:/Users/Elève/Desktop/EasyViz/")
